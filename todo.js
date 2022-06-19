fe1a348c165235f9a3907748fba8170d75a6eea8
hari-str <haristr5235@gmail.com>
2022-06-19 16:13:25 +0530

    :memo:todo list

---
+++ todo.js
@@ -0,0 +1,35 @@
+let todoText = document.getElementById("todo-text");
+let todoUl = document.getElementById('todo-ul');
+
+function addTask() {
+    if (todoText.value.trim() == '') {
+        alert('Enter some text');
+    }
+    else {
+        let liTag = document.createElement('li');
+        let spanText = document.createElement('span');
+
+        // text Node create
+        let textNode = document.createTextNode(todoText.value.trim());
+        spanText.appendChild(textNode);
+
+        // EventListener
+        liTag.addEventListener('click', (e) => {
+            e.target.classList.toggle('done');
+        });
+
+        let spanClose = document.createElement('span');
+        spanClose.classList.add('close');
+        spanClose.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+        spanClose.addEventListener('click', (e) => {
+            let li = e.target.parentElement.parentElement;
+            li.parentElement.removeChild(li);
+        });
+        liTag.appendChild(spanText);
+        liTag.appendChild(spanClose);
+
+        todoUl.appendChild(liTag);
+
+        todoText.value = '';
+    }
+}
