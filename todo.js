a4682ea040bf0e26583da492a57bbb29f4464944
hari-str <haristr5235@gmail.com>
2022-06-20 16:37:28 +0530

    :memo:updated

--- todo.js
+++ todo.js
@@ -19,12 +19,24 @@ function addTask() {
         });
 
         let spanClose = document.createElement('span');
-        spanClose.classList.add('close');
-        spanClose.innerHTML = '<i class="fa-solid fa-xmark"></i>';
-        spanClose.addEventListener('click', (e) => {
-            let li = e.target.parentElement.parentElement;
-            li.parentElement.removeChild(li);
-        });
+        // spanClose.classList.add('close');
+        spanClose.className = "close";
+        let spani = document.createElement('i');
+        spani.className = "fa-solid fa-xmark";
+        console.log(spani.setAttribute("onclick", "removebtn(event)"));
+        spanClose.appendChild(spani);
+        // spanClose.innerHTML = '<i onclick="removebtn(event)" class="fa-solid fa-xmark"></i>';
+
+        // spanClose.addEventListener('click', (e) => {
+        //     let li = e.target.parentElement.parentElement;
+
+
+        //     if (confirm("are you Delete") == true) {
+
+        //         li.parentElement.removeChild(li);
+        //     }
+        // });
+
         liTag.appendChild(spanText);
         liTag.appendChild(spanClose);
 
@@ -32,5 +44,12 @@ function addTask() {
         console.log(todoText.value);
         todoText.value = '';
 
+    }
+}
+function removebtn(e) {
+
+    if (confirm("are you Delete") == true) {
+        let li = e.target.parentElement.parentElement;
+        li.parentElement.removeChild(li);
     }
 }
